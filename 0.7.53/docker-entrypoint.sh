d454f394b4552dd6578e2240111beebcfcc3f65e
Wiktor Niesiobedzki <github@vink.pl>
2019-04-21 19:48:00 +0200

    Add logging

--- 0.7.53/docker-entrypoint.sh
+++ 0.7.53/docker-entrypoint.sh
@@ -36,7 +36,7 @@ if [ ! -d /db/db ] ; then
                 echo "Failure downloading updates"
                 exit 0
             fi
-            cat /db/changes.osm | /app/bin/update_database --db-dir=/db/db $META --compression-method=$OVERPASS_COMPRESSION
+            cat /db/changes.osm | /app/bin/update_database --db-dir=/db/db $META --compression-method=$OVERPASS_COMPRESSION  2>&1 | tee -a /db/changes.log
         ) \
         && rm /db/planet /db/changes.osm \
         && cp -r /app/etc/rules /db/db \
