ec1c229d95b998e229f5eaa1f8df525e88d8a091
Wiktor Niesiobedzki <github@vink.pl>
2019-04-21 22:49:23 +0200

    Update update

--- bin/update_overpass.sh
+++ bin/update_overpass.sh
@@ -3,17 +3,28 @@
 (
     set -e
 
-    if [[ "$OVERPASS_META" == "attic" ]] ; then
+    if [ "x$OVERPASS_META" = "xattic" ] ; then
         META="--keep-attic"
-    else
+    elif [ "x$OVERPASS_META" = "xyes" ] ; then
         META="--meta"
+    else
+        META=""
     fi
 
     while `true` ; do
-        /app/venv/bin/pyosmium-get-changes --server $OVERPASS_DIFF_URL -o /db/changes.osm -f /db/replicate_id \
-            && cat /db/changes.osm | /app/bin/update_database --db-dir=/db/db $META --compression-method=$OVERPASS_COMPRESSION \
-            || rm -f /db/changes.osm
-        rm -f /db/changes.osm
+        /app/venv/bin/pyosmium-get-changes --server $OVERPASS_DIFF_URL -o /db/changes.osm -f /db/replicate_id
+        (
+            ! /app/venv/bin/pyosmium-get-changes -O /db/planet.osm.bz2 --server $OVERPASS_DIFF_URL -o /db/changes.osm -f /db/replicate_id
+            OSMIUM_STATUS=$?
+            if [ $OSMIUM_STATUS -eq 1 ]; then
+                echo "There are still some updates remainging"
+            fi
+            if [ $OSMIUM_STATUS -eq 2 ]; then
+                echo "Failure downloading updates"
+                exit 0
+            fi
+            (cat /db/changes.osm | /app/bin/update_database --db-dir=/db/db $META --compression-method=$OVERPASS_COMPRESSION) 2>&1 | tee -a /db/changes.log
+        )
         sleep 60
     done
 ) 2>&1 | tee -a /db/changes.log
