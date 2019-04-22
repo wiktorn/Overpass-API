ee84c918f9b72a139b9f929324075cf75352e466
Wiktor Niesiobedzki <github@vink.pl>
2019-04-22 09:44:40 +0200

    Use update_from_dir

--- 0.7.51/bin/update_overpass.sh
+++ 0.7.51/bin/update_overpass.sh
@@ -13,17 +13,19 @@
 
     while `true` ; do
         (
-            ! /app/venv/bin/pyosmium-get-changes --server $OVERPASS_DIFF_URL -o /db/changes.osm -f /db/replicate_id
-            OSMIUM_STATUS=$?
-            if [ $OSMIUM_STATUS -eq 1 ]; then
-                echo "There are still some updates remainging"
+            if [! -e  /db/diffs/changes.osm ] ; then
+                ! /app/venv/bin/pyosmium-get-changes --server $OVERPASS_DIFF_URL -o /db/diffs/changes.osm -f /db/replicate_id
+                OSMIUM_STATUS=$?
+                if [ $OSMIUM_STATUS -eq 1 ]; then
+                    echo "There are still some updates remainging"
+                fi
+                if [ $OSMIUM_STATUS -eq 2 ]; then
+                    echo "Failure downloading updates"
+                    exit 0
+                fi
             fi
-            if [ $OSMIUM_STATUS -eq 2 ]; then
-                echo "Failure downloading updates"
-                exit 0
-            fi
-            (cat /db/changes.osm | /app/bin/update_database --db-dir=/db/db $META --compression-method=$OVERPASS_COMPRESSION) 2>&1 | tee -a /db/changes.log
-            rm /db/changes.osm
+            /app/bin/update_from_dir --osc-dir /db/diffs/ --db-dir=/db/db $META
+            rm /db/diffs/changes.osm
         )
         sleep 60
     done
