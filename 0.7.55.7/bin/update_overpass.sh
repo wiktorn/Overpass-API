e1c60f76d0076dc31fad837efb70292d9f4c7091
Wiktor Niesiobędzki <github@vink.pl>
2019-05-03 17:08:35 +0200

    0.7.55.7 release

---
+++ 0.7.55.7/bin/update_overpass.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# TODO: split to two files - one with while loop for unattended use
+# one for updating to current state - second called by first
+
+DIFF_FILE=/db/diffs/changes.osm
+
+(
+    set -e
+    if [[ "${OVERPASS_META}" == "attic" ]] ; then
+        META="--keep-attic"
+    elif [[ "${OVERPASS_META}" == "yes" ]] ; then
+        META="--meta"
+    else
+        META=""
+    fi
+
+    if [[ ! -d /db/diffs ]] ; then
+        mkdir /db/diffs
+    fi
+
+    if /app/bin/dispatcher --show-dir | grep -q File_Error ; then
+        DB_DIR="--db-dir=/db/db"
+    else
+        DB_DIR=""
+    fi
+
+    while `true` ; do
+        if [[ ! -e  /db/diffs/changes.osm ]] ; then
+            set +e
+            /app/venv/bin/pyosmium-get-changes -vvv $1 --server "${OVERPASS_DIFF_URL}" -o "${DIFF_FILE}" -f /db/replicate_id
+            OSMIUM_STATUS=$?
+            set -e
+        else
+            echo "/db/diffs/changes.osm exists. Trying to apply again."
+        fi
+        echo /app/bin/update_database "${DB_DIR}" "${META}" --compression-method="${OVERPASS_COMPRESSION}" --map-compression-method="${OVERPASS_COMPRESSION}"
+        cat "${DIFF_FILE}" | /app/bin/update_database "${DB_DIR}" "${META}" --compression-method="${OVERPASS_COMPRESSION}" --map-compression-method="${OVERPASS_COMPRESSION}"
+        rm "${DIFF_FILE}"
+
+        if [[ "${OSMIUM_STATUS}" -eq 3 ]]; then
+            echo "Update finished with status code: ${OSMIUM_STATUS}"
+            break
+        else
+            echo "There are still some updates remaining"
+            continue
+        fi
+        # for now, until pyosmium-get-changes status code gets cleared
+        break
+    done
+) 2>&1 | tee -a /db/changes.log
---
+++ 0.7.55.7/docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eo pipefail
+shopt -s nullglob
+OVERPASS_META=${OVERPASS_META:-no}
+OVERPASS_MODE=${OVERPASS_MODE:-clone}
+OVERPASS_COMPRESSION=${OVERPASS_COMPRESSION:-gz}
+
+if [[ "$OVERPASS_META" == "attic" ]] ; then
+    META="--keep-attic"
+else
+    META="--meta"
+fi
+
+
+if [[ ! -d /db/db ]] ; then
+    if [[ "$OVERPASS_MODE" = "clone" ]]; then
+        mkdir -p /db/db \
+        && /app/bin/download_clone.sh --db-dir=/db/db --source=http://dev.overpass-api.de/api_drolbr/ $META \
+        && cp -r /app/etc/rules /db/db \
+        && chown -R overpass:overpass /db \
+        && echo "Overpass ready, you can start your container with docker start"
+        exit
+    fi
+
+    if [[ "$OVERPASS_MODE" = "init" ]]; then
+        lftp -c "get -c \"$OVERPASS_PLANET_URL\" -o /db/planet.osm.bz2; exit" \
+        && /app/bin/init_osm3s.sh /db/planet.osm.bz2 /db/db /app "--meta=$OVERPASS_META" "--compression-method=$OVERPASS_COMPRESSION --map-compression-method=$OVERPASS_COMPRESSION" \
+        && echo "Database created. Now updating it." \
+        && cp -r /app/etc/rules /db/db \
+        && chown -R overpass:overpass /db \
+        && echo "Updating" \
+        && /app/bin/update_overpass.sh "-O /db/planet.osm.bz2" \
+        && rm /db/planet.osm.bz2 \
+        && chown -R overpass:overpass /db \
+        && echo "Overpass ready, you can start your container with docker start"
+        exit
+    fi
+fi
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
