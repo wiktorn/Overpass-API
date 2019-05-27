fccb8806c009ffb26ba646b0211b10b1d0df9650
Wiktor Niesiobedzki <github@vink.pl>
2019-05-27 23:24:55 +0200

    Fix environment variable name

--- 0.7.54/bin/dispatcher_start.sh
+++ 0.7.54/bin/dispatcher_start.sh
@@ -3,9 +3,9 @@ set -e -o pipefail
 
 DISPATCHER_ARGS=("--osm-base" "--db-dir=/db/db")
 if [[ "${OVERPASS_META}" == "attic" ]] ; then
-  UPDATE_ARGS+=("--attic")
+  DISPATCHER_ARGS+=("--attic")
 elif [[ "${OVERPASS_META}" == "yes" ]] ; then
-  UPDATE_ARGS+=("--meta")
+  DISPATCHER_ARGS+=("--meta")
 fi
 
 find /db/db -type s -print0 | xargs -0 --no-run-if-empty rm && /app/bin/dispatcher "${DISPATCHER_ARGS[@]}"
