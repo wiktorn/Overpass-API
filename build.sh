553f32099f17019c7426eb0f651a6c605da1efe5
Wiktor Niesiobedzki <github@vink.pl>
2021-09-07 11:46:31 +0200

    Simple build script to use instead of Docker Hub builds

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+for i in `ls -d 0.*` ; do (cd $i; docker build -t wiktorn/overpass-api:$i . ; docker push wiktorn/overpass-api:$i ) ; done
+for i in `ls -d 0.* | grep '^[0-9]*\.[0-9]*\.[0-9]*$' | tail -n 1` ; do (docker tag wiktorn/overpass-api:$i wiktorn/overpass-api:latest ; docker push wiktorn/overpass-api:latest ) ; done
+
