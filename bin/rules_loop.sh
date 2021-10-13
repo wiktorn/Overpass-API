00cf78f827e50371f501f2abcb2a965e8f43c192
Wiktor Niesiobedzki <github@vink.pl>
2021-10-13 14:39:52 +0200

    shfmt

--- bin/rules_loop.sh
+++ bin/rules_loop.sh
@@ -17,35 +17,36 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Overpass_API.  If not, see <http://www.gnu.org/licenses/>.
 
-if [[ -z $1  ]]; then
-{
-  echo "Usage: $0 database_dir [desired_cpu_load]"
-  exit 0
-};
+if [[ -z $1 ]]; then
+	{
+		echo "Usage: $0 database_dir [desired_cpu_load]"
+		exit 0
+	}
 fi
 
 CPU_LOAD=${2:-100}
-DB_DIR="`pwd`/$1"
+DB_DIR="$(pwd)/$1"
 
-EXEC_DIR="`dirname $0`/"
+EXEC_DIR="$(dirname $0)/"
 if [[ ! ${EXEC_DIR:0:1} == "/" ]]; then
-{
-  EXEC_DIR="`pwd`/$EXEC_DIR"
-};
+	{
+		EXEC_DIR="$(pwd)/$EXEC_DIR"
+	}
 fi
 
 pushd "$EXEC_DIR"
 
 while [[ true ]]; do
-{
-  START=$(date +%s)
-  echo "`date '+%F %T'`: update started" >>$DB_DIR/rules_loop.log
-  ./osm3s_query --progress --rules <$DB_DIR/rules/areas.osm3s
-  echo "`date '+%F %T'`: update finished" >>$DB_DIR/rules_loop.log
-  WORK_TIME=$(( $(date +%s) - START  ))
-  SLEEP_TIME=$(( WORK_TIME * 100 / CPU_LOAD - WORK_TIME))
-  # let SLEEP_TIME be at least 3 seconds
-  SLEEP_TIME=$(( SLEEP_TIME < 3 ? 3 : SLEEP_TIME))
-  echo "It took $WORK_TIME to run the loop. Desired load is: ${CPU_LOAD}%. Sleeping: $SLEEP_TIME"
-  sleep $SLEEP_TIME
-}; done
+	{
+		START=$(date +%s)
+		echo "$(date '+%F %T'): update started" >>$DB_DIR/rules_loop.log
+		./osm3s_query --progress --rules <$DB_DIR/rules/areas.osm3s
+		echo "$(date '+%F %T'): update finished" >>$DB_DIR/rules_loop.log
+		WORK_TIME=$(($(date +%s) - START))
+		SLEEP_TIME=$((WORK_TIME * 100 / CPU_LOAD - WORK_TIME))
+		# let SLEEP_TIME be at least 3 seconds
+		SLEEP_TIME=$((SLEEP_TIME < 3 ? 3 : SLEEP_TIME))
+		echo "It took $WORK_TIME to run the loop. Desired load is: ${CPU_LOAD}%. Sleeping: $SLEEP_TIME"
+		sleep $SLEEP_TIME
+	}
+done
--- bin/update_overpass_loop.sh
+++ bin/update_overpass_loop.sh
@@ -2,9 +2,9 @@
 
 OVERPASS_UPDATE_SLEEP=${OVERPASS_UPDATE_SLEEP:-60}
 set +e
-while `true` ;  do
-    if [ -n "$OVERPASS_DIFF_URL" ] ; then
-      /app/bin/update_overpass.sh
-    fi
-    sleep "${OVERPASS_UPDATE_SLEEP}"
+while $(true); do
+	if [ -n "$OVERPASS_DIFF_URL" ]; then
+		/app/bin/update_overpass.sh
+	fi
+	sleep "${OVERPASS_UPDATE_SLEEP}"
 done
