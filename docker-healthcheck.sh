e6a5ea89a7ed6cd5fc4ffb1ab18ef2a0e849186d
Kai Inthesky <148130015+Kai-Inthesky@users.noreply.github.com>
2023-10-17 12:13:14 +0000

    Update docker-healthcheck.sh

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -2,6 +2,6 @@
 
 set -e -o pipefail
 
-OVERPASS_HEALTHCHECK=${OVERPASS_HEALTHCHECK:-'curl -qf "http://localhost/api/interpreter?data=\[out:json\];node(1);out;" | jq ".generator" |grep -q Overpass || exit 1'}
+OVERPASS_HEALTHCHECK=${OVERPASS_HEALTHCHECK:-'curl --noproxy "*" -qf "http://localhost/api/interpreter?data=\[out:json\];node(1);out;" | jq ".generator" |grep -q Overpass || exit 1'}
 
 eval "${OVERPASS_HEALTHCHECK}"
